222ee23dc182e1a90350db06a925d29d411d1d89
osnirfrancisco07-pixel <osnirfrancisco07@gmail.com>
2026-03-10 15:53:13 -0300

    admim.js

--- js/admim.js
+++ js/admim.js
@@ -1,1 +1,14 @@
+function publicar(){
 
+let titulo = document.getElementById("titulo").value;
+let texto = document.getElementById("texto").value;
+let categoria = document.getElementById("categoria").value;
+
+let posts = JSON.parse(localStorage.getItem("posts")) || [];
+
+posts.unshift({
+titulo:titulo,
+texto:texto,
+categoria:categoria
+});
+  
