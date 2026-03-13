977aa86ca12f37c7cb58825523ca381547638eca
osnirfrancisco07-pixel <osnirfrancisco07@gmail.com>
2026-03-13 01:20:48 -0300

    Update script.js
    Fix typo in function declaration.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function toggleMenu() {
+eufunction toggleMenu() {
     const navLinks = document.getElementById('nav-links');
     // Adiciona ou remove a classe 'active' para mostrar/esconder o menu
     navLinks.classList.toggle('active');
